5f6778bdca5ee0a23d87e68c53fa296e7bd21a5b
Andrew Woodward <ebenwoodward@icloud.com>
2018-07-19 05:53:28 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,10 +13,9 @@ end
 def assign_rooms(array)
   str = ""
   array.each_with_index do |names, i|
-    str << "Hello, #{names}! You'll be assigned to room #{i+1}! 
-    "
+    str << "Hello, #{names}! You'll be assigned to room #{i+1}! "
   end
-  return arr
+  return str
 end
     
   
